925e31a81166b0470aa23dc700b96d40fe883478
brenddamachado <brenda16machado.bm@gmail.com>
2023-03-17 18:06:53 -0300

    atualizando o footer

--- segundo-projeto/src/app/app.module.ts
+++ segundo-projeto/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RodapeComponent
+    RodapeComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
